4febd8d2174aeb10d78b0489c71d6766e0645753
KamrulHasan <kamrulhasan3288@gmail.com>
2017-12-21 21:30:14 +0600

    facebook login implemented

--- app/src/main/java/com/kamrulhasan3288/facebookloginapp/AccountActivity.java
+++ app/src/main/java/com/kamrulhasan3288/facebookloginapp/AccountActivity.java
@@ -1,13 +1,59 @@
 package com.kamrulhasan3288.facebookloginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.login.LoginManager;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private TextView accountId,phoneNumberText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        accountId = findViewById(R.id.profileId);
+        phoneNumberText = findViewById(R.id.profilePhoneNumber);
+
+        if (getSupportActionBar()!= null){
+            getSupportActionBar().setTitle("Account");
+        }
+
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+
+                accountId.setText("Account ID : "+account.getId());
+
+                if (account.getPhoneNumber()!= null){
+                    phoneNumberText.setText("Phone Number : "+String.valueOf(account.getPhoneNumber()));
+                }else {
+                    phoneNumberText.setText("Email : "+String.valueOf(account.getEmail()));
+                }
+
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+
+            }
+        });
+    }
+
+    public void onLogout(View view) {
+        AccountKit.logOut();
+        LoginManager.getInstance().logOut();
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/kamrulhasan3288/facebookloginapp/LoginActivity.java
+++ app/src/main/java/com/kamrulhasan3288/facebookloginapp/LoginActivity.java
@@ -1,13 +1,125 @@
 package com.kamrulhasan3288.facebookloginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static int APP_REQUEST_CODE = 99;
+    private LoginButton fbLoginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fbLoginButton = findViewById(R.id.facebook_login_button);
+        fbLoginButton.setReadPermissions("email");
+
+        //login button callback registration
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                launchAccountActivity();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"You close this",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"Server Error occur",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // check for an access token account kit
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        // check for an access token for facebook button
+        com.facebook.AccessToken loginToken = com.facebook.AccessToken.getCurrentAccessToken();
+        if (accessToken != null || loginToken != null){
+            launchAccountActivity();
+        }
+
     }
+
+
+    public void loginWithPhoneNumber(View view) {
+        onLogin(LoginType.PHONE);
+    }
+
+    public void loginWithEmail(View view) {
+        onLogin(LoginType.EMAIL);
+    }
+
+
+    private void onLogin(LoginType loginType) {
+        // crate intent for create the account kit activity
+        final Intent intent = new Intent(LoginActivity.this, AccountKitActivity.class);
+
+        //configure login type and response type
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(loginType, AccountKitActivity.ResponseType.TOKEN);
+
+        final AccountKitConfiguration configuration = configurationBuilder.build();
+
+        //launch account kit activity
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configuration);
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //forward result to the callback manager for login button
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+
+        // confirm that this response matches your request
+        if (requestCode == APP_REQUEST_CODE) {
+
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+
+            if (loginResult.getError() != null) {
+                // display login error
+                String message = loginResult.getError().getErrorType().getMessage();
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+            } else if (loginResult.getAccessToken() != null) {
+                // successful proceed to the account activity
+                launchAccountActivity();
+            }
+
+
+        }
+
+    }
+
+
+    private void launchAccountActivity() {
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
